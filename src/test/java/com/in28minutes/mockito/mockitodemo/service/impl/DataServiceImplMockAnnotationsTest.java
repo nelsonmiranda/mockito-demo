892e3984136d48fb4aef125d9888aed5e1e0e1f9
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-03 16:31:25 -0600

    Test using mockito annotations

---
+++ src/test/java/com/in28minutes/mockito/mockitodemo/service/impl/DataServiceImplMockAnnotationsTest.java
@@ -0,0 +1,38 @@
+package com.in28minutes.mockito.mockitodemo.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.in28minutes.mockito.mockitodemo.service.DataService;
+
+@ExtendWith(MockitoExtension.class)
+public class DataServiceImplMockAnnotationsTest {
+
+	@Mock
+	DataService dataServiceMock;
+	
+	@InjectMocks
+	DataServiceImpl dataService;
+	
+	@Test
+	void findTheGreatestFromAllData() {
+		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {24,16,12});
+		int result = dataService.findTheGreatestFromAllData();
+		assertEquals(24, result);		
+	}
+
+	//We can easily create another scenario
+	@Test
+	void findTheGreatestFromAllData_ForOneValue() {
+		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {15});
+		int result = dataService.findTheGreatestFromAllData();
+		assertEquals(15, result);		
+	}
+}
