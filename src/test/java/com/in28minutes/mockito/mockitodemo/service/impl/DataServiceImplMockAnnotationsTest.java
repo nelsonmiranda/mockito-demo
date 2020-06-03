9bfc66d9041ff273fab4101d521242264433660b
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-03 16:50:10 -0600

    Mocking list interface

--- src/test/java/com/in28minutes/mockito/mockitodemo/service/impl/DataServiceImplMockAnnotationsTest.java
+++ src/test/java/com/in28minutes/mockito/mockitodemo/service/impl/DataServiceImplMockAnnotationsTest.java
@@ -1,7 +1,6 @@
 package com.in28minutes.mockito.mockitodemo.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
