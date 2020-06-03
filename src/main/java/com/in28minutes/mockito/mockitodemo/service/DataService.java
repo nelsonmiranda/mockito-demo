361d904cc48372c45335bf28118da042667e58dd
Nelson Miranda <nelsonmiranda8421@gmail.com>
2020-06-03 11:09:30 -0600

    Implementing services

---
+++ src/main/java/com/in28minutes/mockito/mockitodemo/service/DataService.java
@@ -0,0 +1,7 @@
+package com.in28minutes.mockito.mockitodemo.service;
+
+public interface DataService {
+
+	int[] retrieveAllData();
+	
+}
---
+++ src/main/java/com/in28minutes/mockito/mockitodemo/service/impl/DataServiceImpl.java
@@ -0,0 +1,22 @@
+package com.in28minutes.mockito.mockitodemo.service.impl;
+
+import com.in28minutes.mockito.mockitodemo.service.DataService;
+
+public class DataServiceImpl {
+
+	private DataService dataService;
+	
+	int findTheGreatestFromAllData() {
+		int[] data = dataService.retrieveAllData();
+		int greatest = Integer.MIN_VALUE;
+		
+		for (int value : data) {
+			if(value > greatest) {
+				greatest = value;
+			}
+		}
+		
+		return greatest;
+	}
+	
+}
